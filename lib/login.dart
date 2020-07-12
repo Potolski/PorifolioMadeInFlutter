8e8ec7a64ca9fb4fb6ed4cb9a31779987d9461b3
DavidPotolski <david@brickabode.com>
2020-07-12 13:29:51 -0300

    Adds auth

--- lib/login.dart
+++ lib/login.dart
@@ -1,72 +1,98 @@
 import 'package:flutter/material.dart';
 import 'firstpage.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
   final TextStyle fieldsStyle = TextStyle(color: Colors.grey[50]);
+
+  final _formKey = GlobalKey<FormState>();
+
+  String _username;
+  String _password;
+
   final OutlineInputBorder fieldsborder = OutlineInputBorder(
       borderRadius: BorderRadius.circular(32),
       borderSide: BorderSide(color: Colors.grey[50], width: 0.0));
 
+  fieldsdecoration(String hinttext) {
+    return InputDecoration(
+        hintText: hinttext,
+        hintStyle: fieldsStyle,
+        contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        enabledBorder: fieldsborder,
+        border: fieldsborder);
+  }
+
+  void validateAndSave(_username, _password) {
+    final FormState form = _formKey.currentState;
+    form.save();
+    if (form.validate() && _username == "123" && _password == "302") {
+      print('Form is valid');
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => FirstPage()));
+    } else {
+      print('Form is invalid');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final usernamefield = TextField(
+    final usernamefield = TextFormField(
       obscureText: false,
       style: fieldsStyle,
-      decoration: InputDecoration(
-          hintText: "Username",
-          hintStyle: fieldsStyle,
-          hoverColor: Colors.red,
-          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-          enabledBorder: fieldsborder,
-          border: fieldsborder),
+      validator: (value) => value.isEmpty ? 'Username can\'t be empty' : null,
+      onSaved: (value) => _username = value.trim(),
+      decoration: fieldsdecoration("Username"),
     );
-    final passwordfield = TextField(
+    final passwordfield = TextFormField(
       obscureText: true,
-      decoration: InputDecoration(
-          hintText: "Password",
-          hintStyle: fieldsStyle,
-          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-          enabledBorder: fieldsborder,
-          border: fieldsborder),
+      style: fieldsStyle,
+      validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+      onSaved: (value) => _password = value.trim(),
+      decoration: fieldsdecoration("Password"),
     );
 
-    final loginbutton = Material(
-      color: Colors.red,
-      elevation: 5,
-      borderRadius: BorderRadius.circular(32),
-      child: MaterialButton(
-          minWidth: MediaQuery.of(context).size.width,
-          padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-          child: Text("Valson",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-          onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => FirstPage()));
-          }),
-    );
+    final loginbutton = MaterialButton(
+        color: Colors.red,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(32),
+        ),
+        elevation: 5,
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        child: Text("Valson",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        onPressed: () {
+          validateAndSave(_username, _password);
+        });
 
     return Scaffold(
-        body: Center(
-      child: Container(
-        color: Colors.black,
-        child: Center(
+      body: Form(
+          key: _formKey,
+          child: Container(
+            color: Colors.black,
             child: Padding(
-          padding: const EdgeInsets.all(32.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("Salson", style: TextStyle(color: Colors.red, fontSize: 60)),
-              SizedBox(height: 45),
-              usernamefield,
-              SizedBox(height: 25),
-              passwordfield,
-              SizedBox(height: 25),
-              loginbutton,
-            ],
-          ),
-        )),
-      ),
-    ));
+              padding: const EdgeInsets.all(32.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text("Salson",
+                      style: TextStyle(color: Colors.red, fontSize: 60)),
+                  SizedBox(height: 45),
+                  usernamefield,
+                  SizedBox(height: 25),
+                  passwordfield,
+                  SizedBox(height: 25),
+                  loginbutton,
+                ],
+              ),
+            ),
+          )),
+    );
   }
 }
